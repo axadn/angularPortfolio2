7d937f0bff46b0d09fa19c56e359f1e42ed8cfc1
Adan Marrufo <adanmarrufo@gmail.com>
2024-02-22 14:22:35 -1000

    make projects link to project-detail page

--- portfolio-app/src/app/app.routes.ts
+++ portfolio-app/src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
 
 export const routes: Routes = [
     
     {path: 'about', component: AboutComponent},
     {path: 'contact', component: ContactComponent},
     {path: 'projects', component: ProjectsComponent},
+    {path: 'projects/:id', component: ProjectDetailComponent},
     { path: '**', component: AboutComponent},
 ];
--- portfolio-app/src/app/projects/projects.component.ts
+++ portfolio-app/src/app/projects/projects.component.ts
@@ -12,12 +12,14 @@ export class ProjectsComponent {
   projectItems: ProjectItem[] = [{
     title: 'placeholder',
     previewSrc: 'assets/Apple.webp',
-    skills: ['3d modeling', 'software development']
+    skills: ['3d modeling', 'software development'],
+    id: '1'
   },
   {
     title: 'placeholder',
     previewSrc: 'assets/Apple.webp',
-    skills: ['stuff', 'skill']
+    skills: ['stuff', 'skill'],
+    id: '2'
 
   }
 ];
@@ -28,4 +30,5 @@ interface ProjectItem{
   title: string;
   previewSrc: string;
   skills: string[];
+  id: string;
 }
