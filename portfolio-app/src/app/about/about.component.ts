4fe469af255a2550a8af7fc91cfb49fe12111f72
Adan Marrufo <adanmarrufo@gmail.com>
2024-02-27 22:06:37 -1000

    implement basic about page

--- portfolio-app/src/app/about/about.component.ts
+++ portfolio-app/src/app/about/about.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { MatButtonModule} from '@angular/material/button';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, RouterModule],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
